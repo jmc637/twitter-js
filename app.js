da41ddfb1277343f2826cb25a6d10d1dfe2fc344
Julius Cassin <kohwoojin@gmail.com>
2017-03-07 11:13:55 -0500

    Finished rendering

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const express = require('express');
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
 const app = express();
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true});
+
+app.listen(3000, function () {
+  console.log('Im awake')
+})
+
+app.use(morgan('tiny'));
+
+app.get('/' , function(req, res, next){
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
+    next();
+})
