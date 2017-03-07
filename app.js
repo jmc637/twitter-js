169ee5fefcb53f5f6d3950547e75e84fc2bf9b98
Julius Cassin <kohwoojin@gmail.com>
2017-03-07 14:56:05 -0500

    Finished forms

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ const routes = require('./routes');
 
 const app = express();
 
-const people = [{name: 'Samir'}, {name: 'Julius'}, {name: 'Omri'}];
 
+// Templating
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
 
 app.listen(3000, function () {
-  console.log('Im awake')
+  console.log('Im awake');
 });
 
-
+// TODO
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', function(){ /* … */ });
+server.listen(3000);
+// TODO
 
 app.use(morgan('tiny'));
 app.use(express.static('public'));
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 const tweetBank = require('../tweetBank');
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render('index', {tweets: tweets});
+  res.render( 'index', { tweets: tweets } );
+
+});
+
+router.post('/tweets', urlencodedParser, function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/users/' + name);
 });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var list = tweetBank.find({'name': name});
-  console.log(list);
-  console.log(tweetBank.list());
-  res.render( 'profile', {name:name, tweets: list});
+  res.render( 'profile', {name:name, tweets: list, showForm: true});
 });
 
 router.get('/tweet/:id', function(req, res) {
   var id = req.params.id;
   var tweets = tweetBank.find({'id': id});
-  console.log(tweets);
-  console.log(tweetBank.list());
   res.render( 'tweet', {tweets: tweets});
 });
 
