1652c352dab133d4e919fd60fd6835cfa54f0e3c
Samir Awuapara <samir8787@gmail.com>
2017-03-07 15:14:52 -0500

    Added Socket.io

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+const socketio = require('socket.io');
 
 const app = express();
 
@@ -11,17 +12,13 @@ app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
 
-app.listen(3000, function () {
+const server = app.listen(3000, function () {
   console.log('Im awake');
 });
 
-// TODO
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', function(){ /* … */ });
-server.listen(3000);
-// TODO
+const io = socketio.listen(server);
+
 
 app.use(morgan('tiny'));
 app.use(express.static('public'));
-app.use('/', routes);
+app.use('/', routes(io));
--- routes/index.js
+++ routes/index.js
@@ -7,32 +7,38 @@ const tweetBank = require('../tweetBank');
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+module.exports = function (io) {
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+    res.render( 'index', { tweets: tweets } );
 
-});
+  });
 
-router.post('/tweets', urlencodedParser, function(req, res) {
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('/users/' + name);
-});
+  router.post('/tweets', urlencodedParser, function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    tweetBank.add(name, text);
+    res.redirect('/users/' + name);
 
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find({'name': name});
-  res.render( 'profile', {name:name, tweets: list, showForm: true});
-});
+  });
 
-router.get('/tweet/:id', function(req, res) {
-  var id = req.params.id;
-  var tweets = tweetBank.find({'id': id});
-  res.render( 'tweet', {tweets: tweets});
-});
+  router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var list = tweetBank.find({'name': name});
+    res.render( 'profile', {name:name, tweets: list, showForm: true});
+  });
 
-module.exports = router;
+  router.get('/tweet/:id', function(req, res) {
+    var id = req.params.id;
+    var tweets = tweetBank.find({'id': id});
+    res.render( 'tweet', {tweets: tweets});
+  });
+
+  return router;
+};
+
+
+
 
 
 
